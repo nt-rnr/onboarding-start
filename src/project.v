887dcf0df2b84914bf9d845ad93581f56800b4d5
nt_rnr <mriahifa@uwaterloo.ca>
2025-12-25 01:03:40 -0500

    Your message

--- src/project.v
+++ src/project.v
@@ -5,7 +5,19 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uwasic_onboarding_MohammadRiahi (
+    assign uio_oe = 8'hFF; // Set all IOs to output  
+
+  // Add this inside the module block
+  assign uio_oe = 8'hFF; // Set all IOs to output
+  
+  // Create wires to refer to the values of the registers
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
+
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,6 +28,18 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+pwm_peripheral pwm_peripheral_inst (
+  .clk(clk),
+  .rst_n(rst_n),
+  .en_reg_out_7_0(en_reg_out_7_0),
+  .en_reg_out_15_8(en_reg_out_15_8),
+  .en_reg_pwm_7_0(en_reg_pwm_7_0),
+  .en_reg_pwm_15_8(en_reg_pwm_15_8),
+  .pwm_duty_cycle(pwm_duty_cycle),
+  .out({uio_out, uo_out})
+);
+
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
@@ -23,5 +47,6 @@ module tt_um_example (
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+  
 endmodule
